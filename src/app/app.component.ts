ed80bb502c186fad596d10aa792418327da21ba2
nymajalalabadi <nima.jalalabadii@gmail.com>
2024-02-18 10:56:38 +0330

    Add DatingApp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { NavComponent } from './nav/nav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule, FormsModule, CommonModule, NavComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  users: any;
   title = 'DatingApp';
+
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers(){
+    this.http.get("http://localhost:5250/api/user").subscribe(response=>{
+      this.users=response;
+    },error=>{
+      console.log(error);
+    });
+  }
+ 
 }
---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,22 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-nav',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './nav.component.html',
+  styleUrl: './nav.component.css'
+})
+export class NavComponent {
+  
+  model : any = {
+
+  };
+
+  login()
+  {
+    console.log(this.model);
+  }
+}
