16e4bef6e2926b815de8eaa05f619df5574b1b01
nymajalalabadi <nima.jalalabadii@gmail.com>
2024-02-19 12:48:51 +0330

    Add Register and Cansel Method

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,12 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
 
+  register()
+  {
+
+  }
+
+  cansel()
+  {
+
+  }
 }
