9551a5638ec630644a5467fd940538c8d79d4ade
nymajalalabadi <nima.jalalabadii@gmail.com>
2024-02-20 13:04:37 +0330

    Add Routing For Project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessageComponent } from './message/message.component';
 
-export const routes: Routes = [];
+const appRoutes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'members', component: MemberListComponent },
+    { path: 'members/:id', component: MemberDetailComponent },
+    { path: 'lists', component: ListsComponent },
+    { path: 'messages', component: MessageComponent },
+    { path: '**', component: HomeComponent, pathMatch: 'full' }
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(appRoutes)
+    ],
+    exports: [
+        RouterModule
+    ]
+})
+
+export class AppRoutingModule { }
